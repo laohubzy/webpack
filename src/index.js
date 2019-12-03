1236b30cf123fc38eb4c1d057ed4644151e3bf16
虎牙 <huya@huyadeMBP.lan>
2019-12-03 19:30:42 +0800

    feat:初始化项目

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './css/index.css'
+// import image from './img/test.png'
+
+console.log("hello world");
+const root = document.getElementById('root')
+root.innerHTML = 'hello world!'
+
+// var myImage = new Image();
+// myImage.src = image;
+// root.appendChild(myImage);
+
